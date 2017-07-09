a18da20f171b332aaa67d690971d57103e0c03b7
liznomura <lizn@hawaii.edu>
2017-07-08 19:52:57 -1000

    added abstractions

--- js/app.js
+++ js/app.js
@@ -12,12 +12,50 @@
 // oReq.open("GET", "http://swapi.co/api/people/4/");
 // oReq.send();
 
-//Http method object
+
+// *** ABSTRACTION *** //
+// Http method object
+// use this for the first param of startXHR
+// HTTP_METHOD.GET
 const HTTP_METHOD = {
   GET: 'GET'
 };
 
+// function for XHR
+// use this for all XHR calls
+function startXHR(method, source, cb) {
+  let oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', cb);
+  oReq.open(method, source);
+  oReq.send();
+
+  return oReq;
+}
+
+// urls
+// use these for the source
+const broadCityUrl = 'https://www.reddit.com/r/BroadCity.json';
+const advTimeUrl = 'https://www.reddit.com/r/adventuretime.json';
+const partyParrotUrl = 'https://www.reddit.com/r/PartyParrot.json';
+
+
+const navBar = document.createElement('div');
+navBar.className = 'nav-bar';
 const ulNav = document.createElement('ul');
+ulNav.className = 'ul-nav';
+
+
+// **** MY BOARDS **** //
+startXHR(HTTP_METHOD.GET, advTimeUrl, myBoardsListener);
+
+function myBoardsListener() {
+let myBoardsObj = JSON.parse(this.responseText);
+
+}
+
+
+
+
 
 
 //RANDOM DIV
