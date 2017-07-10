b1e6c6edb06fafb8f44a0e5cda900eb13c4e75d9
liznomura <lizn@hawaii.edu>
2017-07-10 13:53:40 -1000

    loaded adventure time data

--- public/js/app.js
+++ public/js/app.js
@@ -46,19 +46,46 @@ navBar.className = 'nav-bar';
 // **** MY BOARDS **** //
 startXHR(HTTP_METHOD.GET, advTimeUrl, myBoardsListener);
 
-const postDiv = document.createElement('div');
-postDiv.className = postDiv;
-const titleDiv = document.createElement('h2');
-const authorDiv = document.createElement('h3');
-const utcDiv = document.createElement('h3');
-const upVotesDiv = document.createElement('h3');
-const descrDiv = document.createElement('p');
 
 function myBoardsListener() {
-let myBoardsObj = JSON.parse(this.responseText);
-console.log(myBoardsObj);
+  let myBoardsObj = JSON.parse(this.responseText);
+  console.log(myBoardsObj);
 
 
+  for(let i = 0; i < myBoardsObj.data.children.length; i++) {
+    const postDiv = document.createElement('div');
+    postDiv.className = 'postDiv';
+    const imageDiv = document.createElement('div');
+    const titleDiv = document.createElement('h2');
+    const authorDiv = document.createElement('h3');
+    const utcDiv = document.createElement('h3');
+    const upVotesDiv = document.createElement('h3');
+    const descrDiv = document.createElement('p');
+    const contentDiv = document.getElementById('content');
+
+    console.log(myBoardsObj.data.children[i].data.thumbnail);
+
+    const possibleThumbnails = ['self', 'nsfw', 'spoiler'];
+
+if(possibleThumbnails.indexOf(myBoardsObj.data.children[i].data.thumbnail) >= 0) {
+  imageDiv.innerHTML = `<img src=https://upload.wikimedia.org/wikipedia/en/3/37/Adventure_Time_-_Title_card.png alt="place-img" height="250px"></img>`;
+} else {
+    imageDiv.innerHTML = `<img src=${myBoardsObj.data.children[i].data.thumbnail} alt="img" height="250px"></img>`;
+  }
+    titleDiv.innerHTML = `${myBoardsObj.data.children[i].data.title}`;
+    authorDiv.innerHTML = `${myBoardsObj.data.children[i].data.author}`;
+    utcDiv.innerHTML = new Date(`${myBoardsObj.data.children[i].data.created_utc}` * 1000);
+    upVotesDiv.innerHTML = `${myBoardsObj.data.children[i].data.ups}`;
+
+    postDiv.appendChild(imageDiv);
+    postDiv.appendChild(titleDiv);
+    postDiv.appendChild(authorDiv);
+    postDiv.appendChild(utcDiv);
+    postDiv.appendChild(upVotesDiv);
+    contentDiv.appendChild(postDiv);
+    console.log('append child');
+  }
+  console.log('after forloop');
 }
 
 
